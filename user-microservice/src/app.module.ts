21763d687d5b33b7c4d185ab9badec5e7078abfd
Fahmi Chaabane <chaabane.fahmi@outlook.fr>
2022-04-20 01:26:45 +0100

    more on env variables

--- user-microservice/src/app.module.ts
+++ user-microservice/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: +configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
+// https://stackoverflow.com/questions/54535867/is-there-a-way-to-use-configservice-in-app-module-ts
